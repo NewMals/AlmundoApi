25dfb1c994e3cf5e5606daba72533a1771ff75d2
Michael Lozano <newmals313@gmail.com>
2018-04-30 01:00:32 -0500

    Saludo de bienvenida

--- Server.js
+++ Server.js
@@ -25,6 +25,14 @@ server.use('/', cors);
 
 server.use('/', controlador);
 
+server.get('/' ,function saludo(req, res){
+    let welcome = 
+
+    "<!DOCTYPE html><html lang='en' dir='ltr'><body><h1>Bienvenido al API de almundo</h1><br><p>Para visualizar los hoteles adicione al final de la url el siguiente endpoint '/Listado'</p></body></html>"
+    
+    res.status(200).send(welcome);
+});
+
 server.listen(PORT, () => {
     console.log('servidor corriendo', PORT);
 });
